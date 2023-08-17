68d984a07cf1f07483ae18df3d3528372ff020c7
iamadityav <iamadityav@gmail.com>
2023-08-17 15:04:01 +0530

    Added_Calendar_Event

--- Screen/DetailScreen.js
+++ Screen/DetailScreen.js
@@ -1,11 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import EventPicker from '../Screen/EventPicker';
 
 const DetailScreen = () => {
   return (
-    <View>
-      <Text>DetailScreen</Text>
-    </View>
+    <>
+      <EventPicker />
+    </>
   );
 };
 
